7e48ecef55d6b1dd257ac466f3f9bac60f825d2d
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-22 11:12:58 -0700

    added startService to use esbuild from within the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
 const el = document.getElementById("root");
@@ -8,6 +9,18 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
