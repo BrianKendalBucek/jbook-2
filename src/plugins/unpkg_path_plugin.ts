0bdc69429a6e74874578c7880059dce483e0f32d
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-24 14:59:02 -0700

    redirecting path to unpkg

--- src/plugins/unpkg_path_plugin.ts
+++ src/plugins/unpkg_path_plugin.ts
@@ -1,26 +1,33 @@
-import * as esbuild from 'esbuild-wasm';
- 
+import * as esbuild from "esbuild-wasm";
+
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
-      });
- 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        if (args.path === 'index.js') {
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-text-pkg") {
           return {
-            loader: 'jsx',
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        }
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
             contents: `
               import message from 'tiny-test-pkg';
               console.log(message);
             `,
           };
-        } 
+        }
       });
     },
   };
