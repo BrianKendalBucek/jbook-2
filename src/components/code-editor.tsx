47daf7e6f97fd54115c6f855d4440cfa0dffc14e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 12:30:00 -0700

    overrode monaco-jsx-highlighter with  types.d.ts file

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -3,6 +3,8 @@ import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -20,6 +22,10 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     });
 
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    // const highlighter = new Highlighter(
+
+    // );
   };
 
   const onFormatClick = () => {
@@ -33,8 +39,8 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
       plugins: [parser],
       useTabs: false,
       semi: true,
-      singleQuote: true
-    });
+      singleQuote: true,
+    }).replace(/\n$/, '');
 
     // set the formatted value back in the editor
     editorRef.current.setValue(formatted);
