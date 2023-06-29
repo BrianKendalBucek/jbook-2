1b1fe7af223aed197aa1377779909401ba6d8103
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 13:34:55 -0700

    installed react-resizable to work with React v18

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom/client";
 import CodeCell from "./components/code-cell";
-const el = document.getElementById("root");
 
+const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
