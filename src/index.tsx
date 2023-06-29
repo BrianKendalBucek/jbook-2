8023c9c11f9852d0252e21a1030e29f50f4dd6a9
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 13:32:29 -0700

    refactored code from index.tsx into code-cell

--- src/index.tsx
+++ src/index.tsx
@@ -2,35 +2,18 @@
 // npm install --save-exact @monaco-editor/react@3.7.5 --legacy-peer-deps
 
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useState } from "react";
 import ReactDOM from "react-dom/client";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundle from "./bundler";
-
+import CodeCell from "./components/code-cell";
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
+  
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
