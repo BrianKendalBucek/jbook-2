5246590792c709123681c72f7b2854b0fdf7599b
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 09:20:36 -0700

    added type definitions

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,11 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor theme='dark' language='javascript' height="500px"/>;
+  return <MonacoEditor 
+    theme="dark" 
+    language="javascript" 
+    height="500px" 
+    />;
 };
 
 export default CodeEditor;
