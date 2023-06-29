d437f0a4610eff2663e0d36ccc99dbd53de55df4
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 13:22:53 -0700

    moved all relevant bundler files into bundler folder

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,6 +1,6 @@
 import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "../plugins/unpkg_path_plugin";
-import { fetchPlugin } from "../plugins/fetch-plugin";
+import { unpkgPathPlugin } from "./plugins/unpkg_path_plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 let service: esbuild.Service;
 
