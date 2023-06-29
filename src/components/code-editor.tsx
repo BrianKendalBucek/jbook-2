005d845152bf8d6c1d980d9e0416cdc9b39b100d
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 09:29:52 -0700

    added asthetic monaco editor options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -5,6 +5,16 @@ const CodeEditor = () => {
     theme="dark" 
     language="javascript" 
     height="500px" 
+    options={{
+      wordWrap: 'on',
+      minimap: { enabled: false },
+      showUnused: false,
+      folding: false,
+      lineNumbersMinChars: 3,
+      fontSize: 16,
+      scrollBeyondLastLine: false,
+      
+    }}
     />;
 };
 
