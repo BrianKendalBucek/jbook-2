d4d820de7693b04515ba25e18af5009b4760fe7e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-29 10:31:42 -0700

    added bulmanswatch superhero theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 // NEED TO PASS THE --legacy-peer-deps FLAG TO EVERY NPM INSTALL COMMAND THAT IS MADE GOING FORWARD IN THIS COURSE
 // npm install --save-exact @monaco-editor/react@3.7.5 --legacy-peer-deps
 
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
