9f2a1d1ebc3c74f712b71c4ec07c4e5e218afc6d
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-30 15:45:05 -0700

    defined min constraints

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -7,7 +7,7 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
- return <ResizableBox  maxConstraints={[Infinity, window.innerHeight * 0.9]} height={300} width={Infinity} resizeHandles={["s"]}>{children}</ResizableBox>;
+ return <ResizableBox  maxConstraints={[Infinity, window.innerHeight * 0.9]} minConstraints={[Infinity, 24]} height={300} width={Infinity} resizeHandles={["s"]}>{children}</ResizableBox>;
 };
 
 export default Resizable;
