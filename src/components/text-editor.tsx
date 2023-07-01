422eb7f7a040e523f4d0ba6b8f836d4f610a5a88
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-01 10:32:36 -0700

    corrected type cursor by overriding bulma with css

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,23 +1,33 @@
-import { useState, useEffect } from "react";
+import './text-editor.css';
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
       setEditing(false);
     };
-    document.addEventListener('click', listener, { capture: true });
+    document.addEventListener("click", listener, { capture: true });
 
     return () => {
-      document.removeEventListener('click', listener, { capture: true });
+      document.removeEventListener("click", listener, { capture: true });
     };
   }, []);
 
   if (editing) {
     return (
-      <div>
+      <div ref={ref}>
         <MDEditor />
       </div>
     );
