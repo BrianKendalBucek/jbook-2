f38d276996f18ad2cc7b4f10ec474e884a4cb233
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-03 14:30:41 -0700

    added some basic tests to store.ts now commented out

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,27 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+// import { ActionType } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: 'code',
+//   }
+// });
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: 'text',
+//   }
+// });
+
+// how to get specific cell info
+// const id = store.getState().cells.order[0]
+
+// console.log(store.getState());
