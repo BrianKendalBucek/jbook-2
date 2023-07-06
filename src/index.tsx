e204df29330a2fda7e18f3ebb31fe731fcadbde3
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-06 16:58:46 -0700

    corrected Provider error from react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
-// NEED TO PASS THE --legacy-peer-deps FLAG TO EVERY NPM INSTALL COMMAND THAT IS MADE GOING FORWARD IN THIS COURSE
-// npm install --save-exact @monaco-editor/react@3.7.5 --legacy-peer-deps
-
-import "bulmaswatch/superhero/bulmaswatch.min.css";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./state";
-import CellList from "./components/cell-list";
-
-const el = document.getElementById("root");
-const root = ReactDOM.createRoot(el!);
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './state';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <CellList/>
+        <CellList />
       </div>
     </Provider>
   );
 };
 
-root.render(<App />);
+ReactDOM.render(<App />, document.querySelector('#root'));
