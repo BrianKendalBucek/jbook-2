779de6ef747ed95881dbd566f2c21c8c00d0e105
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-09 15:15:04 -0700

    added Preview logic for bundle.code and bundle.err

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -3,11 +3,13 @@ import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
 interface BundlesState {
-  [key: string]: {
-    loading: boolean;
-    code: string;
-    err: string;
-  };
+  [key: string]:
+    | {
+        loading: boolean;
+        code: string;
+        err: string;
+      }
+    | undefined;
 }
 
 const initialState: BundlesState = {};
@@ -18,16 +20,16 @@ const reducer = produce(
       case ActionType.BUNDLE_START:
         state[action.payload.cellId] = {
           loading: true,
-          code: '',
-          err: ''
+          code: "",
+          err: "",
         };
         return state;
       case ActionType.BUNDLE_COMPLETE:
         state[action.payload.cellId] = {
           loading: false,
           code: action.payload.bundle.code,
-          err: action.payload.bundle.err
-        }
+          err: action.payload.bundle.err,
+        };
         return state;
       default:
         return state;
