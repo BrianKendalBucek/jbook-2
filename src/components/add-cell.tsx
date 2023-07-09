d294d46a5c0d81e441f87076de593f249d9cf941
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-08 23:02:36 -0700

    added positioning and spacing of code and text buttons

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -9,9 +9,15 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div className="add-cell" >
-      <button onClick={() => insertCellBefore(nextCellId, "code")}>Code</button>
-      <button onClick={() => insertCellBefore(nextCellId, "text")}>Text</button>
+    <div className="add-cell">
+      <div className="add-buttons">
+        <button onClick={() => insertCellBefore(nextCellId, "code")}>
+          Code
+        </button>
+        <button onClick={() => insertCellBefore(nextCellId, "text")}>
+          Text
+        </button>
+      </div>
       <div className="divider"></div>
     </div>
   );
