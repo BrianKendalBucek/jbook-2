b01a58fd9593436a644fc3ebc419abec9fdf0468
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-10 21:43:31 -0700

    created cumulativeCode array with show() functionality

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,7 +18,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
 
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+        const show = (value) => {
+          document.querySelector('#root').innerHTML = value;
+        };
+      `
+    ];
     for (let c of orderedCells) {
       if (c.type === 'code') {
         cumulativeCode.push(c.content);
@@ -29,8 +35,6 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     }
     return cumulativeCode;
   });
-
-  console.log(cumulativeCode);
 
   useEffect(() => {
     if (!bundle) {
